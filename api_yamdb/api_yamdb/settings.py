c2ffcf5b458c2cff3d619a63269bef9acb4207d1
Casp1an <fxng10@mail.ru>
2022-10-23 22:47:36 +0300

    qwerty

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -135,4 +135,4 @@ SIMPLE_JWT = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
-DEFAULT_FROM_EMAIL = 'auth@yamdb.ru'
+DEFAULT_FROM_EMAIL = 'Yamdb.ru <admin@yamdb.ru>'
